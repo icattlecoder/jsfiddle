ff0decf3715404b7956aabe3d6982260160563e0
icattlecoder <icattlecoder@gmail.com>
2014-01-03 11:29:47 +0800

    fix

--- ajaxupload/demo.js
+++ ajaxupload/demo.js
@@ -12,15 +12,15 @@
  *
  *   实际开发中，您可以通过后端开发语言动态生成这个表单，将token的hidden属性设置为true并对其进行赋值。
  *
- *	**********************************************************************************
- * 	* 贡献代码：
- *	* 1. git clone git@github.com:icattlecoder/jsfiddle
- *	* 2. push代码到您的github库
+ *  **********************************************************************************
+ *  * 贡献代码：
+ *  * 1. git clone git@github.com:icattlecoder/jsfiddle
+ *  * 2. push代码到您的github库
  *   * 2. 测试效果，访问 http://jsfiddle.net/gh/get/extjs/4.2.0/<Your GitHub Name>/jsfiddle/tree/master/<Demo Name>
- *	* 3. 提pr
+ *  * 3. 提pr
  *   **********************************************************************************
  */
-$(document).ready(function(){
+$(document).ready(function() {
     var Qiniu_UploadUrl = "http://up.qiniu.com";
     $("#btn_upload").click(function() {
         //普通上传
@@ -47,7 +47,10 @@ $(document).ready(function(){
                         formatSpeed = uploadSpeed.toFixed(2) + "Kb\/s";
                     }
                     var percentComplete = Math.round(evt.loaded * 100 / evt.total);
-                    console && console.log(percentComplete, ",", formatSpeed);
+                    $("#progressbar").progressbar({
+                        value: percentComplete
+                    });
+                    // console && console.log(percentComplete, ",", formatSpeed);
                 }
 
             }, false);
@@ -55,7 +58,7 @@ $(document).ready(function(){
             xhr.onreadystatechange = function(response) {
                 if (xhr.readyState == 4 && xhr.status == 200 && xhr.responseText != "") {
                     var blkRet = JSON.parse(xhr.responseText);
-                    console&&console.log(blkRet);
+                    console && console.log(blkRet);
                 } else if (xhr.status != 200 && xhr.responseText) {
 
                 }
@@ -64,10 +67,10 @@ $(document).ready(function(){
             xhr.send(formData);
         };
         var token = $("#token").value;
-        if ($("#file").files.length > 0 && token != "") {
-            Qiniu_upload($("#file").files[0], token, $("#key").value);
-        }else{
-        	console&&console.log("form input error");
+        if ($("#file")[0].files.length > 0 && token != "") {
+            Qiniu_upload($("#file")[0].files[0], token, $("#key").value);
+        } else {
+            console && console.log("form input error");
         }
     })
 })

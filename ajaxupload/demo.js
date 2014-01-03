aa7d205b2a486fee82217fe4196a333ef15e3b23
icattlecoder <icattlecoder@gmail.com>
2014-01-03 10:55:38 +0800

    fix ajaxupload js

--- ajaxupload/demo.js
+++ ajaxupload/demo.js
@@ -1,25 +1,73 @@
 /*
-*   本示例演示七牛云存储表单上传
-*
-*   按照以下的步骤运行示例：
-*
-*   1. 填写token。需要您不知道如何生成token，可以点击右侧的链接生成，然后将结果复制粘贴过来。
-*   2. 填写key。如果您在生成token的过程中指定了key，则将其输入至此。否则留空。
-*   3. 姓名是一个自定义的变量，如果生成token的过程中指定了returnUrl和returnBody，
-*      并且returnBody中指定了期望返回此字段，则七牛会将其返回给returnUrl对应的业务服务器。
-*      callbackBody亦然。
-*   4. 选择任意一张照片，然后点击提交即可 
-*   
-*   实际开发中，您可以通过后端开发语言动态生成这个表单，将token的hidden属性设置为true并对其进行赋值。
-*
-*	**********************************************************************************
-* 	* 贡献代码：
-*	* 1. git clone git@github.com:icattlecoder/jsfiddle
-*	* 2. push代码到您的github库
-*   * 2. 测试效果，访问 http://jsfiddle.net/gh/get/extjs/4.2.0/<Your GitHub Name>/jsfiddle/tree/master/<Demo Name>
-*	* 3. 提pr
-*   **********************************************************************************
-*/
-$(function(){
-	alert("hello")
+ *   本示例演示七牛云存储表单上传
+ *
+ *   按照以下的步骤运行示例：
+ *
+ *   1. 填写token。需要您不知道如何生成token，可以点击右侧的链接生成，然后将结果复制粘贴过来。
+ *   2. 填写key。如果您在生成token的过程中指定了key，则将其输入至此。否则留空。
+ *   3. 姓名是一个自定义的变量，如果生成token的过程中指定了returnUrl和returnBody，
+ *      并且returnBody中指定了期望返回此字段，则七牛会将其返回给returnUrl对应的业务服务器。
+ *      callbackBody亦然。
+ *   4. 选择任意一张照片，然后点击提交即可
+ *
+ *   实际开发中，您可以通过后端开发语言动态生成这个表单，将token的hidden属性设置为true并对其进行赋值。
+ *
+ *	**********************************************************************************
+ * 	* 贡献代码：
+ *	* 1. git clone git@github.com:icattlecoder/jsfiddle
+ *	* 2. push代码到您的github库
+ *   * 2. 测试效果，访问 http://jsfiddle.net/gh/get/extjs/4.2.0/<Your GitHub Name>/jsfiddle/tree/master/<Demo Name>
+ *	* 3. 提pr
+ *   **********************************************************************************
+ */
+$(function() {
+    var Qiniu_UploadUrl = "http://up.qiniu.com";
+    $("#btn_upload").click(function() {
+        //普通上传
+        var Qiniu_upload = function(f, token, key) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', Qiniu_UploadUrl, true);
+            var formData, startDate;
+            formData = new FormData();
+            if (key !== null && key !== undefined) formData.append('key', key);
+            formData.append('token', token);
+            formData.append('file', f);
+            var taking;
+            xhr.upload.addEventListener("progress", function(evt) {
+                if (evt.lengthComputable) {
+                    var nowDate = new Date().getTime();
+                    taking = nowDate - startDate;
+                    var x = (evt.loaded) / 1024;
+                    var y = taking / 1000;
+                    var uploadSpeed = (x / y);
+                    var formatSpeed;
+                    if (uploadSpeed > 1024) {
+                        formatSpeed = (uploadSpeed / 1024).toFixed(2) + "Mb\/s";
+                    } else {
+                        formatSpeed = uploadSpeed.toFixed(2) + "Kb\/s";
+                    }
+                    var percentComplete = Math.round(evt.loaded * 100 / evt.total);
+                    console && console.log(percentComplete, ",", formatSpeed);
+                }
+
+            }, false);
+
+            xhr.onreadystatechange = function(response) {
+                if (xhr.readyState == 4 && xhr.status == 200 && xhr.responseText != "") {
+                    var blkRet = JSON.parse(xhr.responseText);
+                    console&&console.log(blkRet);
+                } else if (xhr.status != 200 && xhr.responseText) {
+
+                }
+            };
+            startDate = new Date().getTime();
+            xhr.send(formData);
+        };
+        var token = $("$token").value;
+        if ($("#file").files.length > 0 && token != "") {
+            Qiniu_upload($("#file").files[0], token, $("#key").value);
+        }else{
+        	console&&console.log("form input error");
+        }
+    })
 })()
